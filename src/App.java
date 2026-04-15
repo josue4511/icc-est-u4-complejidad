dd756875aa84a768d3c701ca2bf43ccf2a8d73cc
josue4511 <ccallei@est.ups.edu.ec>
2026-04-14 19:37:46 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[]array = {1, 2, 3, 4, 5};
+        System.out.println(array);
+        System.out.println(array[0]);
+
+        // for Normal
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+
+        // for-each
+        for(int i: array){
+            System.out.println(i);
+            
+        }
     }
 }
